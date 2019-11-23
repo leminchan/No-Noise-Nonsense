f2f78a3d1116d2306acbf730aff42a9722c08d30
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-11-22 20:09:46 -0800

    Version 0.1
    Successfully logs the average noise level in the room every second
    If the noise level exceeds a threshold over a given period of time,
    a buzzer will trigger.

--- main/main.ino
+++ main/main.ino
@@ -1,54 +1,84 @@
 /*
-//LED anode (long leg) attached to RED_LED
-//LED cathode (short leg) attached to ground
-//Note:
-This example code is in the public domain.
-http://www.seeedstudio.com/depot/Grove-Sound-Sensor-p-752.html?cPath=25_128
+No Noise Nonsense
+
+Created by Chase Carnaroli and Mit Chan
+For Computer Science 141 - Taught by Sergio Gago-Masague
 */
-#define SOUND_SENSOR 1 /* sound sensor pin */
-#define LED 12 /* LED pin */
-#define THRESHOLD_VALUE 600 /* Depending on your LaunchPad’s ADC resolution you may
-have to adjust the threshold */
-#define ON HIGH /* led on */
-#define OFF LOW /* led off */
-#define _handle_led(x) digitalWrite(LED, x) /* handle led */
-/* Global Variables */
-unsigned long sound_value = 0;
-unsigned long count = 0;
-unsigned long sound_value_sum = 0;
-unsigned long average_sound_value;
-unsigned long timer = millis() + 7000; // 10 = measure every 10 seconds
 
-void setup() {
+#define SOUND_SENSOR 1 
+#define BUZZER 6
+#define THRESHOLD_VALUE 100 
+#define ON HIGH
+#define OFF LOW
 
- /* Initialize led pin */
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
+#define ONE_SECOND 1000 
+#define FIFTEEN_SECONDS 15000
+
+unsigned long one_second_timer;
+unsigned long fifteen_second_timer;
+
+long sound_level_per_sec_avg = 0;
+unsigned long sound_level_per_sec_count = 0;
+long sound_level_per_fifteen_sec_avg = 0;
+unsigned long sound_level_per_fifteen_sec_count = 0;
+
+void setup() 
+{
+  /* Initialize led pin */
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, OFF);
   Serial.begin(9600);
-}
-void loop() {
 
-  if (timer < millis())
+  one_second_timer = millis() + ONE_SECOND;
+  fifteen_second_timer = millis() + FIFTEEN_SECONDS;
+}
+
+long addToAvg(long value, long avg, long count) 
+{
+  //Serial.print(value);Serial.print(" avg: ");Serial.print(avg);Serial.print(" count: ");Serial.print(count);Serial.print(" result: ");Serial.println(avg + (value-avg) / count);
+  return avg + (value-avg) / count;
+}
+
+void loop() {
+  if (one_second_timer < millis())
   {
-    average_sound_value = sound_value_sum / count;
-    Serial.print("avg sound value: ");
-    Serial.println(average_sound_value);
-    delay(2000);
-    timer = millis() + 7000;
-    count = 0;
+    Serial.print(millis()/1000);
+    Serial.print(": ");
+    Serial.println(sound_level_per_sec_avg);
+
+    sound_level_per_fifteen_sec_count++;
+    sound_level_per_fifteen_sec_avg = addToAvg(sound_level_per_sec_avg, sound_level_per_fifteen_sec_avg, sound_level_per_fifteen_sec_count);
+    
+    sound_level_per_sec_avg = 0;
+    sound_level_per_sec_count = 0;
+   
+    one_second_timer = millis() + ONE_SECOND;
+  }
+
+  if (fifteen_second_timer < millis())
+  {
+    Serial.println("-------------------------------");
+    Serial.print("15 Second Avg: ");
+    Serial.println(sound_level_per_fifteen_sec_avg);
+    Serial.println("-------------------------------");
+
+    //If the noise level exceeds the threshold, trigger the buzzer
+    if (sound_level_per_fifteen_sec_avg > THRESHOLD_VALUE)
+    {
+      digitalWrite(BUZZER, ON);
+      delay(1000);
+      digitalWrite(BUZZER, OFF);
+      delay(200);
+    }
+
+    sound_level_per_fifteen_sec_avg = 0;
+    sound_level_per_fifteen_sec_count = 0;
+
+    fifteen_second_timer = millis() + FIFTEEN_SECONDS;
   }
   
-  /* read the sound value */
-  sound_value = analogRead(SOUND_SENSOR);
-  Serial.println(sound_value);
-
-  sound_value_sum += sound_value;
+  // Read sound level, add it to sound_level_per_sec_avg
+  sound_level_per_sec_count++;
+  sound_level_per_sec_avg = addToAvg(analogRead(SOUND_SENSOR), sound_level_per_sec_avg, sound_level_per_sec_count);
   
-  /* if the value is larger than threshold, turn on led */
-  if(sound_value > THRESHOLD_VALUE) {
-    _handle_led(ON);
-    //delay(1000);
- }
-  _handle_led(OFF);
-  count++;
 }
