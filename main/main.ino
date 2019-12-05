44891473b73459105f6d0fc2ad5b096173d1a5f3
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-12-05 01:38:02 -0800

    Takes average sound level every 50 milliseconds

--- main/main.ino
+++ main/main.ino
@@ -7,12 +7,14 @@ For Computer Science 141 - Taught by Sergio Gago-Masague
 
 #define SOUND_SENSOR 1 
 #define BUZZER 6
-#define THRESHOLD_VALUE 100 
+#define THRESHOLD_VALUE 200 
 #define ON HIGH
 #define OFF LOW
 
-#define ONE_SECOND 1000 
+#define ONE_SECOND 50 
 #define FIFTEEN_SECONDS 15000
+
+int max_level = 0;
 
 unsigned long one_second_timer;
 unsigned long fifteen_second_timer;
@@ -27,7 +29,7 @@ void setup()
   /* Initialize led pin */
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, OFF);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   one_second_timer = millis() + ONE_SECOND;
   fifteen_second_timer = millis() + FIFTEEN_SECONDS;
@@ -39,28 +41,37 @@ long addToAvg(long value, long avg, long count)
   return avg + (value-avg) / count;
 }
 
+void sendLong(long value) 
+{
+  //Serial.write(int(value) >> 8);    // send the high byte
+  //Serial.write(int(value) & 0xFF);  // send the low byte
+}
+
 void loop() {
+
   if (one_second_timer < millis())
   {
-    Serial.print(millis()/1000);
-    Serial.print(": ");
+    //Serial.print(millis()/1000);
+    //Serial.print(": ");
     Serial.println(sound_level_per_sec_avg);
+
+    sendLong(sound_level_per_sec_avg);
 
     sound_level_per_fifteen_sec_count++;
     sound_level_per_fifteen_sec_avg = addToAvg(sound_level_per_sec_avg, sound_level_per_fifteen_sec_avg, sound_level_per_fifteen_sec_count);
     
     sound_level_per_sec_avg = 0;
     sound_level_per_sec_count = 0;
-   
+
     one_second_timer = millis() + ONE_SECOND;
   }
-
+  
   if (fifteen_second_timer < millis())
   {
-    Serial.println("-------------------------------");
-    Serial.print("15 Second Avg: ");
-    Serial.println(sound_level_per_fifteen_sec_avg);
-    Serial.println("-------------------------------");
+    //Serial.println("-------------------------------");
+    //Serial.print("15 Second Avg: ");
+    //Serial.println(sound_level_per_fifteen_sec_avg);
+    //Serial.println("-------------------------------");
 
     //If the noise level exceeds the threshold, trigger the buzzer
     if (sound_level_per_fifteen_sec_avg > THRESHOLD_VALUE)
@@ -79,6 +90,8 @@ void loop() {
   
   // Read sound level, add it to sound_level_per_sec_avg
   sound_level_per_sec_count++;
-  sound_level_per_sec_avg = addToAvg(analogRead(SOUND_SENSOR), sound_level_per_sec_avg, sound_level_per_sec_count);
+  long sound_level = analogRead(SOUND_SENSOR);
+  sound_level_per_sec_avg = addToAvg(sound_level, sound_level_per_sec_avg, sound_level_per_sec_count);
+  delay(10);
   
 }
